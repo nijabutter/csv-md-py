984e3e621560fe0b4a91921a9d9f20a7645e4136
nijabutter <nijabutter@gmail.com>
2021-04-22 13:52:09 +0100

    Clean up

--- table.py
+++ table.py
@@ -1,22 +1,36 @@
 import sys
 
-if len(sys.argv) != 2:
-    print("Usage: table.py <file-name.csv>")
+delimiter = ","
+
+if len(sys.argv) != 2 and len(sys.argv) != 4:
+    print("Usage: table.py FILE [-d ,]")
     sys.exit(0)
 
-with open(sys.argv[1]) as f:
-    
-    contents = f.readlines()
-    longest = [0] * len(contents[0].split(","))
+for i in range(len(sys.argv)):
+    if sys.argv[i] == "-d":
+        i += 1
+        delimiter = sys.argv[i][0]
+    else:
+        file_name = sys.argv[i]
 
-    for r in range(len(contents)):
-        row = contents[r].split(",")
-        for c in range(len(row)):
-            if len(row[c]) > longest[c]:
-                longest[c] = len(row[c])
+with open(file_name) as f:    
+    contents = f.readlines()
+    if not contents:
+        exit(1)
+
+    longest = [0] * len(contents[0].split(delimiter)) # split the first line to get the number of columns
+
+    for i, line in enumerate(contents):
+        line = line.split(delimiter)
+        if len(line) > len(longest):
+            print(f"Line {i} contains {len(line)} delimiters instead of {len(line)}:")
+            print(delimiter.join(line))
+            exit(1)
+        for j, col in enumerate(line):
+            longest[j] = max(longest[j], len(col))
     
     seperator = ""
-    firstRow = contents[0].split(",")
+    firstRow = contents[0].split(delimiter)
     for l in range(len(longest)):
         seperator += "-" * longest[l]
         print(firstRow[l].replace("\n", "").ljust(longest[l]),end="")
@@ -26,7 +40,7 @@ with open(sys.argv[1]) as f:
     print("\n" + seperator)
 
     for row in contents[1:]:
-        row = row.split(",")
+        row = row.split(delimiter)
         for l,col in enumerate(row):
             col = col.replace("\n", "")
             print(col.ljust(longest[l]), end="")
