d15c19c777934f79cb7ef99b3b29248f193760d3
nijabutter <67209342+nijabutter@users.noreply.github.com>
2021-04-26 11:58:11 +0100

    Fix typo. Add todo

--- table.py
+++ table.py
@@ -1,29 +1,38 @@
 import sys
-
+'''
+TODO:
+- add -p (pretty) option to enable padding for the text e.g
+    -p:
+    |header|header    |header |
+    |abc   |abcdefghij|ab     |
+    |abcdef|abc       |abcdefg|
+- redirect from stdin and stdout if - is used
+'''
 delimiter = ","
 
 if len(sys.argv) != 2 and len(sys.argv) != 4:
     print("Usage: table.py FILE [-d ,]")
     sys.exit(0)
-
+  
 for i in range(len(sys.argv)):
-    if sys.argv[i] == "-d":
+    if sys.argv[i] == "-d" and i < len(sys.argv)-1:
         i += 1
         delimiter = sys.argv[i][0]
     else:
         file_name = sys.argv[i]
 
-with open(file_name) as f:    
+with open(file_name) as f:
     contents = f.readlines()
     if not contents:
-        exit(1)
+        exit(1) # output empty table instead ?
 
     longest = [0] * len(contents[0].split(delimiter)) # split the first line to get the number of columns
+    # will be used later to store the longest (char len) column in each row to add padding
 
     for i, line in enumerate(contents):
         line = line.split(delimiter)
         if len(line) > len(longest):
-            print(f"Line {i} contains {len(line)} delimiters instead of {len(line)}:")
+            print(f"Line {i} contains {len(line)} delimiters instead of {len(longest)}:")
             print(delimiter.join(line))
             exit(1)
         for j, col in enumerate(line):
